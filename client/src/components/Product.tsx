0a59b8e22495f504e8f13af0ff2414f1e35e6d98
Maria Antonietta Guevara Garcia <m4ri4.guev4r4@gmail.com>
2022-12-11 20:31:14 -0500

    Mostramos un error cuando el producto no existe.

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,37 +1,47 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { List } from "./List";
-import { products } from "../data/products";
+import {useParams} from "react-router-dom";
+import {List} from "./List";
+import {getPublicPath, products} from "../data/products";
 
 function Product() {
-  const { id } = useParams();
-  // TODO
-  const product = products.find((p) => p.id.toString() === id);
+    const {id} = useParams();
+    // TODO
+    const product = products.find((p) => p.id.toString() === id);
 
-  return (
-    <>
-        <div className="row pt-4">
-            <div className="col">
-                <img src={product?.imagen} className="card-img" alt="..."/>
-            </div>
-            <div className="col">
-                <h1>Price: ${product?.precio}</h1>
-                <h2>Nombre: { product?.nombre }</h2>
-                <p>Detalle: { product?.descripcion }</p>
-                <span className="badge text-bg-warning">{product?.category}</span>
-                <br />
-                <div className="p-2">
-                    <button className="btn btn-success">Agregar +</button>
-                </div>
-            </div>
-        </div>
+    return (
+        <>
+            {(typeof product !== "undefined") &&
+                <>
+                    <div className="row pt-4">
+                        <div className="col">
+                            <img src={getPublicPath(product.imagen)} className="card-img" alt="..."/>
+                        </div>
+                        <div className="col">
+                            <h1>Price: ${product.precio}</h1>
+                            <h2>Nombre: {product.nombre}</h2>
+                            <p>Detalle: {product.descripcion}</p>
+                            <span className="badge text-bg-warning">{product.category}</span>
+                            <br/>
+                            <div className="p-2">
+                                <button className="btn btn-success">Agregar +</button>
+                            </div>
+                        </div>
+                    </div>
 
-        <div className="pt-4">
-            <h2>Productos relacionados ({product?.category})</h2>
-            <List products={products} cat={product?.category} />
-        </div>
-    </>
-  );
+                    <div className="pt-4">
+                        <h2>Productos relacionados ({product.category})</h2>
+                        <List products={products} cat={product.category}/>
+                    </div>
+                </>
+            }
+
+            {(typeof product === "undefined") &&
+                <>
+                    <h1 className={"text-center mt-3"}>El producto no existe.</h1>
+                </>
+            }
+        </>
+    );
 }
 
-export { Product };
+export {Product};
