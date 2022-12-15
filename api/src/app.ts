0be25d69a0610c192580707288c433d519b256f8
manu-andaluz <manupeandaluz@gmail.com>
2022-12-15 17:26:09 -0300

    Creando ruta para obtener los productos y eliminando las imagenes ( ya subidas a cloudinary )

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,13 +4,11 @@ import config from './config'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
-import multer from 'multer'
-import imgModel from './routes/models/imagesModel'
+// import multer from 'multer'
 import dotenv from 'dotenv'
-import productsRoutes from './routes/products.routes'
-import fs from 'fs'
-import path, { dirname } from 'path'
-import uploads from '../src/uploads'
+// import fs from 'fs'
+// import path, { dirname } from 'path'
+import product from './routes/models/ProductsModel'
 
 dotenv.config()
 const app = express()
@@ -26,58 +24,51 @@ app.use(express.urlencoded({ extended: true })) // para que pueda entender los c
 app.use(bodyParser.json())
 app.set('view engine', 'ejs') // se ve en el vistas carpeta para las plantillas que hacen.
 
-app.use(productsRoutes)
-
 // seteames el  images storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads')
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.fieldname + '-' + Date.now())
+//   }
+// })
 
 // controlador para la solicitud GET a nuestro servidor. La respuesta muestra una página HTML
 // que muestra todas las imágenes almacenadas en la base de datos y proporciona una interfaz
 // de usuario para cargar nuevas imágenes.
-const upload = multer({ storage })
+// const upload = multer({ storage })
 
 // cargamos el modelo de las imágenes
 
-app.get('/', (req, res) => {
-  imgModel.find({}, (err: any, items: any) => {
-    if (err) {
-      console.log(err)
-      res.status(500).send('An error occurred')
-    } else {
-      res.render('imagesPage', { items }) // o res.render("index")
-    }
+app.get('/products', (req, res) => {
+  product.find({}).then((product) => {
+    res.json(product)
   })
 })
 
 // Maneje la solicitud POST que procesa los datos del formulario enviados por el usuario
 // desde nuestra interfaz de usuario HTML. Esta solicitud hará que se carguen
 // las nuevas imágenes.
-app.post('/', upload.single('image'), (req, res, next) => {
-  const obj = {
-    name: req.body.name,
-    desc: req.body.desc,
-    img: {
-      // eslint-disable-next-line n/no-path-concat
-      data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)), 
-      contentType: 'image/png'
-    }
-  }
-  imgModel.create(obj, (err, item) => {
-    if (err) {
-      console.log(err)
-    } else {
-      item.save()
-      console.log('Saved to database')
-      res.redirect('/')
-    }
-  })
-})
+// app.post('/', upload.single('image'), (req, res, next) => {
+//   const obj = {
+//     name: req.body.name,
+//     desc: req.body.desc,
+//     img: {
+//       // eslint-disable-next-line n/no-path-concat
+//       data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)), 
+//       contentType: 'image/png'
+//     }
+//   }
+//   imgModel.create(obj, (err, item) => {
+//     if (err) {
+//       console.log(err)
+//     } else {
+//       item.save()
+//       console.log('Saved to database')
+//       res.redirect('/')
+//     }
+//   })
+// })
 
 export default app
