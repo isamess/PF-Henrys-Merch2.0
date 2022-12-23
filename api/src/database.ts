eb3b0d3b661334c4bf62e86e6f2c2aad1d207b37
unknown <liadesben@gmail.com>
2022-12-22 23:11:28 -0300

    shopping cart

--- api/src/database.ts
+++ api/src/database.ts
@@ -4,8 +4,8 @@ import config from "./config";
 (async () => {
   try {
     const mongooseOptions: ConnectOptions = {
-      user: config.MONGO_USER, //usuario por defecto de esta DB
-      pass: config.MONGO_PASSWORD,
+      //   user: config.MONGO_USER, //usuario por defecto de esta DB
+      //   pass: config.MONGO_PASSWORD,
     };
     const db = await mongoose.connect(
       `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@henrycomerce.3etyvbd.mongodb.net/test`,
