33f471bd22d11cd84b331a720224d4b0e34690e2
Nicolas <nicoferni55@gmail.com>
2023-01-09 11:57:06 -0300

    deploy

--- api/src/config.ts
+++ api/src/config.ts
@@ -7,7 +7,8 @@ export default {
     MONGO_PASSWORD: process.env.MONGO_PASSWORD ||'admin', 
     MONGO_HOST: process.env.MONGO_HOST ||'localhost',
     PORT: process.env.PORT || 3001,
-    MONGO_URI: process.env.MONGO_URI
+    MONGO_URI: process.env.MONGO_URI,
+    MONGO_DEPLOY: process.env.MONGO_DEPLOY
 }
     export const CLOUDINARY_CLOUD_NAME= process.env.CLOUDINARY_CLOUD_NAME;
     export const CLOUDINARY_API_KEY= process.env.CLOUDINARY_API_KEY;
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import app from './app'
 import './database'
+require('dotenv').config();
 
 app.listen(app.get('port'), () => {
   console.log('server on port', app.get('port'))
