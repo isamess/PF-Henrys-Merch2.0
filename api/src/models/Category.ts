8bef6e86410b8fcfa7b6499c83f33fd84aa64765
Isa Mesina <isabelmesina@gmail.com>
2023-01-08 23:11:14 -0300

    categories Model

---
+++ api/src/models/Category.ts
@@ -0,0 +1,25 @@
+import {Schema, model, PaginateModel, Document} from 'mongoose'
+const mongoosePaginate= require ('mongoose-paginate-v2')
+import {category} from '../utils/types'
+
+
+const categorySchema = new Schema({
+    name: String,
+  });
+
+  categorySchema.plugin(mongoosePaginate);
+  
+  // modifica el _id de lo que devuelve la DB por id y remueve el __v
+  categorySchema.set("toJSON", {
+    transform: (_document, returnedObject) => {
+      returnedObject.id = returnedObject._id;
+      delete returnedObject._id;
+      delete returnedObject.__v;
+    },
+  });
+  
+  interface CategoryDocument extends Document, category {}
+  
+  
+  
+  export const Category = new (model(CategoryDocument, PaginateModel<CategoryDocument>))('Categories', categorySchema, 'categories');
