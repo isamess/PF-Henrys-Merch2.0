e04032dcfa8c8991b2d593a2d52493845ca64161
unknown <liadesben@gmail.com>
2023-01-10 21:17:13 -0300

    fixes

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../redux/slices/CartSlice";
 
-//PASO LAS PROPS
+// PASO LAS PROPS
 const List = (category: any) => {
   const dispatch = useDispatch();
   const { products }: any = useSelector((state: any) => state.products);
@@ -21,13 +21,13 @@ const List = (category: any) => {
   return (
     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mt-2">
       {products.map((product: any) =>
-        product.category[0] === categoryName ? (
+        product.category === categoryName ? (
           <div className="d-flex justify-content-center" key={product._id}>
             <div className="card my-card">
               <img src={product.image} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
-                {/* <p className="card-text">{product.desc}</p> */}
+                <p className="card-text">{product.desc}</p>
                 <p className="card-text">desc</p>
                 <p className="card-text">
                   ${product.price.toFixed(2).toLocaleString()}
